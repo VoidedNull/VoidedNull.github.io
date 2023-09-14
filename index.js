94e13bd5bd526d29791a9635ab494c74639d32f6
VoidedNull <101037575+VoidedNull@users.noreply.github.com>
2023-09-14 14:26:04 +0300

    Light mode and Dark mode switch

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function OpenInNewTabWinBrowser() {
     var win = window.open('https://github.com/VoidedNull/', '_blank');
     win.focus();
 }
+
+function Dark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
