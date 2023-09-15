5ee79b830b078a92490f34c548c3119971636231
VoidedNull <101037575+VoidedNull@users.noreply.github.com>
2023-09-15 12:43:19 +0300

    final product i hope

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function Dark() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+ const checkbox = document.getElementById('input');
+ const svg1 = document.getElementById('moon');
+ const svg2 = document.getElementById('sun');
+ 
+ checkbox.addEventListener('change', function() {
+   if (this.checked) {
+     svg1.classList.add('invisible'); // Add the 'invisible' class to svg1
+     svg2.classList.add('invisible'); // Add the 'invisible' class to svg2
+   } else {
+     svg1.classList.remove('invisible'); // Remove the 'invisible' class from svg1
+     svg2.classList.add('invisible'); // Add the 'invisible' class to svg2
+   }
+ });
